2c0608a0ff2bc83b9f6a584ecb6926e5c41b7f3e
Cuauhtemoc Rodriguez <crodri606@gmail.com>
2019-10-02 16:39:24 -0500

    working on design

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -11,22 +11,9 @@ class NavBar extends Component{
                   <NavLink smooth to="#contact" className="text-dark nav-link" activeClassName="active">
                     Contact
                   </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink exact smooth to="#about" className="text-dark nav-link">
-                    <a>About</a>
-                  </NavLink> 
-                </li>
-                <li className="nav-item">
-                  <NavLink smooth to="#projects" className="text-dark nav-link" activeClassName="active">
-                    Projects
-                  </NavLink>
-                </li>
-                {/* <li className="nav-item">
-                  <a href={Resume} target="_blank" className="text-dark nav-link" activeClassName="active">
-                    Resume
-                  </a>
-                </li> */}
+              </li>
+           
+            
               </ul>
             </div>
           </nav>
